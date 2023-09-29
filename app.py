670987dfb01a4e68678cf703036bd1684ae5d498
Pieter Bons <p.c.bons@hva.nl>
2023-09-29 13:27:36 +0200

    remove plotly

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import geopandas as gpd
-import plotly.express as px
 from streamlit_folium import folium_static
 import folium
 import matplotlib.cm as cm
