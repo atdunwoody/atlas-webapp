917cca60af24256e37605195753af3698fd40732
Pieter Bons <p.c.bons@hva.nl>
2023-09-29 13:23:28 +0200

    loopplezier app werkend versie 1

---
+++ app.py
@@ -0,0 +1,127 @@
+import streamlit as st
+import pandas as pd
+import geopandas as gpd
+import plotly.express as px
+from streamlit_folium import folium_static
+import folium
+import matplotlib.cm as cm
+import matplotlib.colors as mcolors
+from functions import *
+
+@st.cache_data
+def load_data():
+	gdf = gpd.read_feather('./data/df_full.feather').to_crs('EPSG:4326')
+	gdf = gdf.reset_index(drop = True)
+	
+	nodes = gpd.read_feather('./data/nodes.feather').to_crs('EPSG:4326')
+	nodes = nodes.reset_index().rename(columns = {'osmid' : 'knooppunt'})
+	return (gdf, nodes)
+
+def style_function(feature):
+	cmap = cm.RdYlGn  # Choose a continuous colormap (11 colors)
+	value = feature['properties']['Score']  # Get the value from your column
+	color = mcolors.rgb2hex(cmap(value))  # Map the value to a color
+	return {'color': color}
+
+def calculate_new_column(gdf, ovl, bomen, water, monumenten, wegen, parken):
+    # Add your calculation logic here, e.g., using min_value and max_value
+	gdf['Score'] = (gdf['score_ovl']*ovl + 
+					gdf['score_bomen']*bomen + 
+					gdf['score_water']*water + 
+					gdf['score_monumenten']*monumenten + 
+					gdf['score_wegen']*wegen + 
+					gdf['score_park']*parken)
+
+	return gdf	
+
+#@st.cache_resource
+def create_map(_gdf, _nodes, _df_route = None, route = False):
+	m = folium.Map(location=[_gdf['geometry'].centroid.y.mean(), _gdf['geometry'].centroid.x.mean()], zoom_start=14)
+
+	folium.GeoJson(
+		_gdf,
+		name='Score',
+		style_function=style_function).add_to(m)	
+	
+	folium.GeoJson(
+		_nodes,
+		name='Nodes',
+		marker = folium.CircleMarker(radius = 2, # Radius in metres
+                                           weight = 0, #outline weight
+                                           fill_color = '#000000', 
+                                           fill_opacity = 1),
+		tooltip=folium.GeoJsonTooltip(fields=['knooppunt'], labels=True, sticky=True)
+		).add_to(m)	
+	
+	if route: 
+		folium.GeoJson(
+			_df_route,
+			name='Route').add_to(m)	
+		
+	return m
+
+def calculate_route(gdf, start, end, min, max):
+
+	a = gdf.sort_values(['u','v']).pivot(index = 'u', columns = 'v', values = 'length').fillna(100000).values
+	s = gdf.sort_values(['u','v']).pivot(index = 'u', columns = 'v', values = 'Score').fillna(0).values
+	s=s*a
+
+	s_norm = s.copy()
+	s_norm -= np.min(s_norm)
+	s_norm /= np.max(s_norm)
+ 
+	best_solution, distance, score, runtime = looproutes_ant_colony_optimization(a,s,s_norm,start,end,min,max)
+	
+	#route = route_corine = [2921, 2775, 3095, 3097, 2139, 2068, 2019, 2007, 2006, 1977, 1980, 1969, 1978, 1941, 1926, 1853, 1825, 1847, 2498, 1738, 1681, 1682, 1658, 1623, 1615, 1581, 1558, 1557, 1560, 2537, 1539, 2757, 1523, 1498, 1495, 1497, 1479, 1446, 1441, 1445, 1420, 1438, 1419, 1384, 1346, 1321, 1302, 1263, 1253, 1222, 1220, 1221, 2866, 1276, 1184, 1168, 1183, 1199, 1227, 2655, 1193, 2913]
+	df_route = pd.DataFrame({'u' : best_solution})
+	df_route['v'] = df_route.u.shift(-1)
+	df_route = df_route.dropna()
+	df_route = gdf.merge(df_route)
+	return df_route
+
+def main():
+	# Title and description
+	st.title("Loopplezier kaart")
+	
+	(gdf, nodes) = load_data()
+	
+	# Sidebar with sliders
+	st.sidebar.header("Map Settings")
+
+	df_route = None
+	route = False
+	calculate_triggered = False
+	
+	ovl = st.sidebar.number_input("Score openbare verlichting", -10,10,0,1,  key="ovl")
+	bomen = st.sidebar.number_input("Score bomen", -10,10,0,1, key="bomen")
+	water = st.sidebar.number_input("Score water", -10,10,0,1, key="water")
+	monumenten = st.sidebar.number_input("Score monumenten", -10,10,0,1, key="monumenten")
+	wegen = st.sidebar.number_input("Score drukke wegen", -10,10,0,1, key="wegen")
+	parken = st.sidebar.number_input("Score parken", -10,10,0,1, key="parken")
+	calculate_button = st.sidebar.button("Calculate")
+	
+	start = st.sidebar.number_input("Start knooppunt", 0,3100,0,1,  key="start")
+	end = st.sidebar.number_input("Eind knooppunt", 0,3100,0,1,  key="end")
+	min_dist = st.sidebar.number_input("Minimale afstand", 0,5000,0,100,  key="min_dist")
+	max_dist = st.sidebar.number_input("Maximale afstand", 0,5000,0,100,  key="max_dist")
+	add_route = st.sidebar.button("Add route")
+		
+	if calculate_button:
+		gdf = calculate_new_column(gdf, ovl, bomen, water, monumenten, wegen, parken)
+		calculate_triggered = True
+
+	if add_route:
+		gdf = calculate_new_column(gdf, ovl, bomen, water, monumenten, wegen, parken)
+		df_route = calculate_route(gdf, start, end, min_dist, max_dist)
+		route = True
+		calculate_triggered = True
+	
+	if calculate_triggered:
+		 folium_static(create_map(gdf, nodes, df_route, route), width=800, height=600)
+
+	calculate_triggered = False
+	route = False
+	
+# Run the app
+if __name__ == '__main__':
+	main()
---
+++ functions.py
@@ -0,0 +1,138 @@
+# packages
+import pulp as plp
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.sparse import csr_matrix
+from scipy.sparse.csgraph import shortest_path
+import time
+import networkx as nx
+import joblib
+
+def probabilitiesMoves(a,s,s_norm,i_c,j_e,g_c,g_min,g_max,tabu,tau, alpha, beta, dist_matrix):
+    # Bepaal naar welke knooppunten een ant kan bewegen en wat de kans is voor elk knooppunt
+    # i_c: huidige node van ant
+    # j_e: eindpunt
+    # g_c: afstand tot nu toe
+    # g_min/gmax: minimale en maximale afstand
+    # tabu: punten die al bezocht zijn
+    
+    possibleMoves = np.where(a[:,i_c]<1000)[0]
+
+    probabilityMoves = np.zeros(len(possibleMoves))
+    
+    for i in range(len(possibleMoves)):
+        j = possibleMoves[i]
+        if j in tabu: # al bezocht 
+            probabilityMoves[i] = 0
+        elif dist_matrix[j] + g_c + a[i_c,j] > g_max: # te lange route tot eindpunt
+            probabilityMoves[i] = 0
+        elif j == j_e and g_c + a[i_c,j] < g_min: # te kort om te stoppen
+            probabilityMoves[i] = 0
+        else:
+            probabilityMoves[i] = tau[i_c,j]**alpha * ((s_norm[i_c,j]+0.1)/a[i_c,j])**beta
+
+    if sum(probabilityMoves) > 0:
+        probabilityMoves /= sum(probabilityMoves)
+
+    return possibleMoves, probabilityMoves
+
+
+def selectMove(probabilities):
+    # trek random item aan de hand van kansverdeling
+    
+    rand = np.random.rand(1)
+    
+    for i in range(len(probabilities)):
+        if sum(probabilities[:(i+1)]) > rand:
+            index = i 
+            break
+    
+    return index
+
+def distanceScoreRoute(a,s,solution):
+    # Bereken de adstand en score gegeven een route
+    
+    distance = 0
+    score = 0 
+    for i in range(len(solution)-1):
+        distance += a[solution[i], solution[i+1]]
+        score += s[solution[i], solution[i+1]]
+        
+    if distance >0:
+        score /= distance
+
+    return distance, score
+	
+def looproutes_ant_colony_optimization(a,s,s_norm,i_s,j_e,g_min,g_max,n_ants = 300,n_iter = 10,alpha = 0.5,beta =0.5,rho = 0.5,Q3 = 1,paths = []):
+    # ant colony optimization om looproutes te bepalen
+    n = a.shape[0]
+    
+    start_time = time.time()
+    # Kortste path van elke route naar eindpunt
+    dist_matrix = shortest_path(csgraph=csr_matrix(a), directed=False, indices=j_e)
+
+    # initialisatie tau
+    # Voor alle i,j zet tau_ij(0) op 1
+
+    tau = np.ones((n,n))
+    #"""
+    for i in range(len(paths)):
+        _,score = distanceScoreRoute(a,s,paths[i])
+        for j in range(len(paths[i])-1):
+            tau[paths[i][j],paths[i][j+1]] += Q3*score
+    #"""
+
+    # beste route bewaren
+    opt_route = []
+    opt_value = 0
+    opt_distance = 0
+
+    # algoritme
+    for i in range(n_iter):
+        # Zorg dat voor alle ants bijgehouden wordt bij welke nodes ze al zijn geweest.
+        # Ze starten allemaal in het startpunt.
+        tabu = []
+
+        current_length = np.zeros(n_ants)
+        current_value = np.zeros(n_ants)
+        value_ant = np.zeros(n_ants)
+
+        for k in range(n_ants):
+            tabu.append([i_s])
+
+        # aantal ants dat eindpunt hebben bereikt
+        end = 0
+
+        # alle mieren bewegen over netwerk tot ze eindpunt bereiken of niet meer verder kunnen.
+        while end < n_ants:  
+            for k in range(n_ants):
+                if tabu[k][-1] != j_e:
+                    possibleMoves, probabilityMoves = probabilitiesMoves(a,s,s_norm,tabu[k][-1],j_e,current_length[k],g_min,g_max,tabu[k],tau, alpha, beta,dist_matrix)
+                    if sum(probabilityMoves) > 0:
+                        move = possibleMoves[selectMove(probabilityMoves)]
+                        current_length[k] += a[tabu[k][-1],move]
+                        current_value[k] += s[tabu[k][-1],move]
+                        tabu[k].append(move)
+                        if move == j_e:
+                            value_ant[k] = current_value[k]/current_length[k]
+                            end += 1
+                    else:
+                        tabu[k].append(j_e)
+                        value_ant[k] = 0
+                        end += 1
+        # update tau   
+        tau *= rho
+        for k in range(n_ants):
+            for i in range(len(tabu[k])-1):
+                tau[tabu[k][i],tabu[k][i+1]] += Q3*value_ant[k]
+
+        # beste route bewaren:
+        if max(value_ant) > opt_value:
+            opt_route = tabu[np.argmax(value_ant)]
+            opt_value = max(value_ant)
+            opt_distance = current_length[np.argmax(value_ant)]
+
+    runtime = time.time()-start_time
+    
+    return opt_route, opt_distance, opt_value, runtime
